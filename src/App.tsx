9c4a56a8fcb3f294a1f5b3350f5d32cdf90a3df1
REBECANONATO <rebecanonato89@gmail.com>
2021-04-29 10:01:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/globals';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
