af29662f1dfb3bb6b50304886b868a27b255def6
REBECANONATO <rebecanonato89@gmail.com>
2021-04-30 17:52:16 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
-      {/* <SignUp /> */}
+      <AuthContext.Provider value={{ name: 'Rebeca' }}>
+        <SignIn />
+      </AuthContext.Provider>
       <GlobalStyle />
     </>
   );
