5e7bfcc557ece2a8c7352c7b6432468a05b75edc
REBECANONATO <rebecanonato89@gmail.com>
2021-05-03 21:35:47 -0300

    animação toast

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
+
+import Toast from './Toast';
+
 import { ToastMessage } from '../../hooks/toast';
 import { Container } from './styles';
-import Toast from './Toast';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+
+
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message: { id: any; }) => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 1 },
+    },
+  );
+
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }: any) => (
+        <Toast key={key} style={props} message={item} />
       ))}
     </Container>
   );
